91f23aff531b3f70970999e7124a284c85cccf18
Shelly <zhangyuyue@coocaa.com>
2016-08-29 17:01:46 +0800

    键盘事件全部写完啊啊啊啊啊啊啊啊

--- js/tv.js
+++ js/tv.js
@@ -1,34 +1,6 @@
 $(document).ready(function($) {
 	$('#inner_left_div').hide();
 
-	//侧边栏事件
-	$('#siderbar-sm').click(function(event) {
-		siderbarShow();
-	});
-
-	$('#siderbar-lg').click(function(event) {
-		siderbarHide();
-	});
-
-	//导航栏事件
-	$('#listen').click(function() {
-		$('#content').children('div').animate({left: 158}, 500, function() {
-			$('#inner_left_div').hide(500);
-		});	
-	});
-	
-	$('#watch').click(function() {
-		$('#inner_left_div').show(50, function() {
-			$('#content').children('div').animate({left: -470}, 500);
-		});
-	});
-
-	$('#play').click(function() {
-		$('#inner_left_div').show(50, function() {
-			$('#content').children('div').animate({left: -1090}, 500);
-		});;
-	});
-
 	//版块选择事件
 	var curSection = 2;
 	var curElement = $('.combination').eq(2);
@@ -40,13 +12,18 @@ $(document).ready(function($) {
         switch (e.keyCode) {
         	case 37: {//左
         		if(curSection == 0) {
-					
+					var tp = curElement.prev();
+					if(tp.length) {
+						curElement.css('border', 'none');
+						curElement = tp;
+						selectSec(curElement, true);
+					}
         		} else if(curSection == 2) {
         			curElement.css('border', 'none');
 
         			if($('.combination').index(curElement) == 0) {
         				curSection = 1
-        				$('#siderbar-sm').click();
+        				siderbarShow();
         				$('#inner_left_div').hide();
         				curElement = $('#siderbar-lg').children('li').eq(0);
         				curElement.css('border', '1px solid red');	
@@ -72,17 +49,30 @@ $(document).ready(function($) {
         		break;
         	}
             case 38: {//上
-            	curElement.css('border', 'none');
             	if(curSection == 1) {
+            		curElement.css('border', 'none');
+            		var tp = curElement.prev();
+            		if(tp.length == 0) {
+            			curSection = 0;
+            			curElement = $('header').find('li').eq(0);
+            			siderbarHide();
+            			selectSec(curElement);
+            		} else {
+            			curElement = tp;
+            			curElement.css('border', '1px solid red');
+            		}
 
             	} else if(curSection == 2) {
+            		curElement.css('border', 'none');
             		var t = curElement.parent().children();
         			if(t.index(curElement) >= 1) {
         				curElement = t.eq(0);
         				selectCbt(curElement);
         			} else {
         				curSection = 0;
-        				
+        				var sec = curElement.parent().attr('class').substr(-1) - 0;
+        				curElement = $('header').find('li').eq(sec);
+        				selectSec(curElement);
         			}
             	}
         		break;
@@ -91,7 +81,12 @@ $(document).ready(function($) {
         		curElement.css('border', 'none');
 
         		if(curSection == 0) {
-
+        			var tn = curElement.next();
+					if(tn.length) {
+						curElement.css('border', 'none');
+						curElement = tn;
+						selectSec(curElement, true);
+					}
         		} else if(curSection == 1){
         			curSection = 2;
         			siderbarHide();
@@ -119,16 +114,40 @@ $(document).ready(function($) {
         		}
         		break;
         	}
-        	case 40: {//下
-        		curElement.css('border', 'none');
+        	case 40: {//下        		
 
         		if(curSection == 0) {
+        			curElement.css('border', 'none');
+        			curSection = 2;
+
+        			var sec = curElement.attr('id');
+        			switch(sec) {
+        				case "listen": {
+        					curElement = $('.section0').children().eq(0);
+        					break;
+        				}
+        				case "watch": {
+        					curElement = $('.section1').children().eq(0);
+        					break;
+        				}
+        				case "play": {
+        					curElement = $('.section2').children().eq(0);
+        					break;
+        				}
+        			}
+        			curElement.css('border', '1px solid red');
 
         		} else if(curSection == 1) {
-
+            		var tn = curElement.next();
+            		if(tn.length != 0) {
+            			curElement.css('border', 'none');
+            			curElement = tn;
+            			curElement.css('border', '1px solid red');
+            		}
         		} else {
         			var t = curElement.parent().children();
         			if(t.index(curElement) == 0) {
+        				curElement.css('border', 'none');
         				curElement = curElement.next();
         				selectCbt(curElement);
         			}
@@ -141,7 +160,6 @@ $(document).ready(function($) {
 
 function selectCbt(curElement) {
 	curElement.css('border', '1px solid red');
-	var l = - (curElement[0].offsetLeft - curElement.width());
 	if($('.combination').index(curElement) == 0) {
 		var l = - (curElement[0].offsetLeft - curElement.width());
 		$('#inner_left_div').hide();
@@ -151,6 +169,29 @@ function selectCbt(curElement) {
 	$('#content').children('div').animate({left: l}, 500);
 }
 
+function selectSec(curElement, isFrom0) {
+	curElement.css('border', '1px solid red');
+	if(isFrom0) {
+		curElement.css('border', '1px solid red');
+		var sec = $('header').find('li').index(curElement);
+		switch(sec) {
+			case 0: {
+				selectListen();
+				break;
+			}
+			case 1: {
+				selectWatch();
+				break;
+			}
+			case 2: {
+				selectPlay();
+				break;
+			}
+		}
+	}
+}
+
+//侧边栏事件
 function siderbarShow() {
 	$('#siderbar-sm').css('visibility', 'hidden');
 	$('#siderbar-lg').animate({left: 0}, 500, function() {
@@ -163,4 +204,23 @@ function siderbarHide() {
 		$('#siderbar-sm').width(95);
 		$('#siderbar-sm').css('visibility', 'visible');
 	});
+}
+
+//导航栏事件
+function selectListen() {
+	$('#content').children('div').animate({left: 158}, 500, function() {
+		$('#inner_left_div').hide(500);
+	});	
+}
+
+function selectWatch() {
+	$('#inner_left_div').show(50, function() {
+		$('#content').children('div').animate({left: -470}, 500);
+	});
+}
+
+function selectPlay() {
+	$('#inner_left_div').show(50, function() {
+		$('#content').children('div').animate({left: -1090}, 500);
+	});;
 }
